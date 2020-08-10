654bdd48c6286b9f22d7b5d2e4048a24948938d5
VictorXiao <843731025@qq.com>
2020-08-10 16:12:55 +0900

    Updates

---
+++ precache-manifest.b01d9fd96e5c27c5630b08db84e56ba8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51a2d5219eec361abcb50838908127c7",
+    "url": "/tweets/index.html"
+  },
+  {
+    "revision": "8e0f8d9cb475f1cd3e02",
+    "url": "/tweets/static/css/2.0c0672e5.chunk.css"
+  },
+  {
+    "revision": "8e0f8d9cb475f1cd3e02",
+    "url": "/tweets/static/js/2.1e31ce8d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tweets/static/js/2.1e31ce8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e6eedc6f767bede3516",
+    "url": "/tweets/static/js/main.85fffc64.chunk.js"
+  },
+  {
+    "revision": "c573d1c4b2bd2b7749d4",
+    "url": "/tweets/static/js/runtime-main.104e0796.js"
+  }
+]);
